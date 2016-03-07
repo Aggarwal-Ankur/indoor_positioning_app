2b640bb64ffc5f225530c50968c9d0031e426834
Ankur Aggarwal <ankur.aggarwal@hsc.com>
2016-03-07 22:28:27 +0530

    Initial project commit

---
+++ main/src/main/java/com/aggarwalankur/indoor_positioning/IPSMainActivity.java
@@ -0,0 +1,13 @@
+package com.aggarwalankur.indoor_positioning;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IPSMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
